5fe66208bb06def1fad0a14a68635628b9a12e0b
zhangwang <zhangwang19961004@users.noreply.github.com>
2017-07-31 19:56:48 +0800

    Create savetoexcel.py

---
+++ savetoexcel.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import xlwt
+import requests
+from bs4 import BeautifulSoup
+wbk = xlwt.Workbook()
+my = wbk.add_sheet('my')
+res=requests.get("http://123.206.41.19")
+res.encoding="utf8"
+soup=BeautifulSoup(res.text,"lxml")
+a=soup.select("a")
+n=0
+for i in a:
+    if(i.text and i.get("href")):
+        my.write(n,0,i.text)
+        my.write(n,1,i.get("href"))
+        n=n+1
+wbk.save('E:/Uploads/test.xls')
